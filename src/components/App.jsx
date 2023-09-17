4186980a52ce35a7a145066e19f76cdc7fdb501f
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-09-17 12:55:18 +0300

    try instal element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import { NavLink, Route, Routes } from "react-router-dom";
+import {Home} from '../pages/Home.page'
+
 
 export const App = () => {
   return (
@@ -12,7 +14,7 @@ export const App = () => {
         </li>
       </ul>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />;
+        <Route path="/" element={<Home/>} />;
         <Route path="/movies" element={<div>Movies</div>} />;
         <Route path="/movies/:movieId"element={<div>Movies items</div>} />;
 
